13134bf3f82ecbff0717418521e6b24d613a8671
Robert Southee <rksouthee@gmail.com>
2020-10-24 09:04:51 +0100

    Basic screen updating

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <Windows.h>
+#include <cassert>
 #include <vector>
 
 static HANDLE screen_handle;
 static int screen_width;
 static int screen_height;
 static std::vector<char> screen_state;
+static int screen_x;
+static int screen_y;
 
 static DWORD screen_initialize()
 {
@@ -34,11 +37,46 @@ static DWORD screen_initialize()
 	return last_error;
 }
 
+static void screen_putchar(char c)
+{
+	assert(screen_x < screen_width && screen_y <= screen_height);
+	if (screen_y == screen_height)
+		return;
+
+	screen_state[screen_y * screen_width + screen_x] = c;
+
+	++screen_x;
+	if (screen_x == screen_width) {
+		++screen_y;
+		screen_x = 0;
+	}
+}
+
+static void screen_putstring(const char* str)
+{
+	while (*str)
+		screen_putchar(*str++);
+}
+
+static void screen_update()
+{
+	DWORD size = static_cast<DWORD>(screen_state.size());
+	DWORD chars_written;
+	COORD cursor{0, 0};
+	// TODO: check success and number of characters written
+	WriteConsoleOutputCharacterA(screen_handle, screen_state.data(), size, cursor, &chars_written);
+	cursor.X = static_cast<SHORT>(screen_x);
+	cursor.Y = static_cast<SHORT>(screen_y);
+	SetConsoleCursorPosition(screen_handle, cursor);
+}
+
 int main()
 {
 	DWORD last_error = screen_initialize();
 
 	if (last_error == 0) {
+		screen_putstring("Hello, world!");
+		screen_update();
 		Sleep(1000);
 	} else {
 		OutputDebugStringA("Failed to initialize screen buffer\n");
